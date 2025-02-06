59c94d95e48ad05beac9037abdc0697e9478941d
Shad <sh4dowswat@gmail.com>
2025-02-06 18:58:43 +0100

    Aggiunti temi

--- WellIo-frontend/src/app/shared/components/dash-settings/dash-settings.component.ts
+++ WellIo-frontend/src/app/shared/components/dash-settings/dash-settings.component.ts
@@ -1,19 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { DashboardService } from '../../services/dashboard.service';
+import {NgClass} from '@angular/common';
+import {ThemeService} from '../../services/theme.service';
 
 @Component({
   selector: 'app-dash-settings',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './dash-settings.component.html',
   styleUrl: './dash-settings.component.css'
 })
 export class DashSettingsComponent implements OnInit {
+  selectedTheme: string = "blue";
 
-  constructor(private dashSerive: DashboardService) {}
+  constructor(private dashSerive: DashboardService,
+              private themeService: ThemeService) {}
 
   ngOnInit(): void {
     this.dashSerive.setHeaderText("Your Settings");
+    this.themeService.themeColor$.subscribe(theme => {
+      this.selectedTheme = theme;
+    })
   }
 
+  setTheme(theme: string): void {
+    this.themeService.setThemeColor(theme);
+  }
 }
